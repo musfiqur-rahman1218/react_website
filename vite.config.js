a3e5c3a7cc3fe04c5c6f9570c8f729e7561730b1
musfiqur-rahman1218 <md.musfiqur.rahman@smu.ca>
2025-10-31 20:42:53 -0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-project/', // 👈 update this to match your new repo name
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/react_website/' : '/',
 })
