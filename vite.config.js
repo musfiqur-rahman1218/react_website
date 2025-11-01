6b889fa5928a2e1a16d00c46a902e05abaa0cf33
musfiqur-rahman1218 <md.musfiqur.rahman@smu.ca>
2025-10-31 21:00:19 -0300

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ✅ Works both locally and on GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/react_website/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react_website/' : '/react-project/',
 })
